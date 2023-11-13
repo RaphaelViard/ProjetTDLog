48b0c9e14f1ce1e62baea712a6d059d86b1c20f2
Raphaël Viard <raphaelviard1@gmail.com>
2023-11-13 15:50:24 +0100

    a

--- Classes/Client.py
+++ Classes/Client.py
@@ -4,17 +4,19 @@ from Ticket import Ticket
 Colors = Literal["RED", "BLUE", "ORANGE", "BROWN", "PURPLE ", "YELLOW", "WHITE", "BLACK"]
 
 
-class Client:
-    def __init__(self : str,password : str,username : str,email : str,nbtickets : int,tickets : list[Ticket], money : float, color : Colors):
+class User:
+    def __init__(self : str,password : str,username : str,email : str,ticketstosale : list[Ticket],ticketowned : list[Ticket], money : float, color : Colors):
         self._password = password
         self.username = username
         self.email = email
-        self.nbtickets = nbtickets  #The number of tickets we are trying to sell
-        self.tickets = tickets #The tickets we are trying to sell
-        self.money = money #Money the user on his account
+        self.ticketstosale = ticketstosale #The tickets we are trying to sell
+        self.ticketowned = ticketowned #The tickets we own (!= tickets we sell)
+        self.money = money #Money the user has on his account
         self.color = color
 
 
     @property.setter
     def password(self,newpassword):
-        return self._password
+        self._password = newpassword
+
+    
