dbc60139c2b4e680862a20956d0f8bc8d6f49138
RaphaelViard <raphaelviard1@gmail.com>
2024-01-24 17:48:34 +0100

    a

--- Flask3 copie/app/models.py
+++ Flask3 copie/app/models.py
@@ -1,14 +1,13 @@
 from app import db
 from flask_login import UserMixin
 from sqlalchemy import Column, String
-import datetime
 
-#from app import db
-#from flask_login import UserMixin
-#from app import login_manager
-#from sqlalchemy import ForeignKeyConstraint, Column, Integer, String, Boolean, Float
-#import datetime
-#from sqlalchemy.ext.declarative import declarative_base
+# from app import db
+# from flask_login import UserMixin
+# from app import login_manager
+# from sqlalchemy import ForeignKeyConstraint, Column, Integer, String, Boolean, Float
+# import datetime
+# from sqlalchemy.ext.declarative import declarative_base
 
 
 # Modèle de données pour la classe User
