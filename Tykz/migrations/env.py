cc6b4dfcb6a808839b1ebcc81517ad6d99d0a93c
RaphaelViard <raphaelviard1@gmail.com>
2024-01-25 11:34:12 +0100

    a

--- Tykz/migrations/env.py
+++ Tykz/migrations/env.py
@@ -33,7 +33,7 @@ def get_engine_url():
 
 
 # add your model's MetaData object here
-# for 'autogenerate' support
+# for "autogenerate" support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 config.set_main_option("sqlalchemy.url", get_engine_url())
@@ -52,12 +52,12 @@ def get_metadata():
 
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
+    """Run migrations in "offline" mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    we don"t even need a DBAPI to be available.
 
     Calls to context.execute() here emit the given string to the
     script output.
@@ -73,7 +73,7 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode.
+    """Run migrations in "online" mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
@@ -97,7 +97,8 @@ def run_migrations_online():
     connectable = get_engine()
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=get_metadata(), **conf_args)
+        context.configure(
+            connection=connection, target_metadata=get_metadata(), **conf_args)
 
         with context.begin_transaction():
             context.run_migrations()
